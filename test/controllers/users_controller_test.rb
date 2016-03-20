8f75fde7851863359cd6cc5fc2ce68ad6dba43cb
Erinna Chen <erinna.chen@gmail.com>
2016-03-20 14:03:34 -0600

    Add pagination to user, refactor index to use user partial

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,7 +10,7 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-  
+
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
