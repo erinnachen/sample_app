5cc2baaa663bad01a67239ebdd2f14f35f749d58
Erinna Chen <erinna.chen@gmail.com>
2016-03-20 14:15:23 -0600

    Add admin can destroy users functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
