13e705b86ce3f0631aab2ea14c8c6dfb07c2700c
Erinna Chen <erinna.chen@gmail.com>
2016-03-20 20:23:17 -0600

    Create following follower relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,16 @@ class User < ActiveRecord::Base
   validates :password, presence: true,
                        length: { minimum: 6 },
                        allow_nil: true
+
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -39,5 +48,17 @@ class User < ActiveRecord::Base
 
   def feed
     microposts
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
